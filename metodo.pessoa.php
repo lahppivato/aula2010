a4e2634bcca770bae296503dfa9862bd5fe972e6
lahppivato <130668198+lahppivato@users.noreply.github.com>
2023-10-20 09:58:02 -0300

    Commit inicial da Laura

---
+++ metodo.pessoa.php
@@ -0,0 +1,371 @@
+<?php
+
+//Classe
+include("nome.class.php");
+
+//Laura Perez Pivato
+$p1 = new Pessoas();
+
+//Atribuir valores
+$p1->nome ="Laura Perez PIvato";
+$p1->altura =1.70;
+$p1->nascimento ="03-08-2006";
+$p1->hobbie = "Ler";
+
+//Pedro Santos
+$p2 = new Pessoas();
+
+//Atribuir valores
+$p2->nome ="Pedro Santos";
+$p2->altura =1.80;
+$p2->nascimento ="24-10-2006";
+$p2->hobbie = "Tocar Violão";
+
+//Samuel Américo
+$p3 = new Pessoas();
+
+//Atribuir valores
+$p3->nome ="Samuel Américo";
+$p3->altura =1.85;
+$p3->nascimento ="26-01-2007";
+$p3->hobbie = "Tocar Piano";
+
+//Júlia Mogi
+$p4 = new Pessoas();
+
+//Atribuir valores
+$p4->nome ="Júlia Mogi";
+$p4->altura =1.70;
+$p4->nascimento ="08-11-2006";
+$p4->hobbie = "Cozinhar";
+
+//Thell Zardi
+$p5 = new Pessoas();
+
+//Atribuir valores
+$p5->nome ="Thell Zardi";
+$p5->altura =1.65;
+$p5->nascimento ="31-10-2006";
+$p5->hobbie = "Jogar";
+
+//Kauã Lira
+$p6 = new Pessoas();
+
+//Atribuir valores
+$p6->nome ="Kauã Lira";
+$p6->altura =1.63;
+$p6->nascimento ="05-01-2007";
+$p6->hobbie = "Tocar guitarra";
+
+//Kauan Silva Vieira
+$p7 = new Pessoas();
+
+//Atribuir valores
+$p7->nome ="Kauan Silva Vieira";
+$p7->altura =1.68;
+$p7->nascimento ="02-09-2006";
+$p7->hobbie = "Jogar videogame";
+
+//Ana Zinatto 
+$p8 = new Pessoas();
+
+//Atribuir valores
+$p8->nome ="Ana Zinatto ";
+$p8->altura =1.60;
+$p8->nascimento =" 03-11-2006";
+$p8->hobbie = "Treinar/ ouvir música";
+
+//Letícia Gabrielle
+$p9 = new Pessoas();
+
+//Atribuir valores
+$p9->nome ="Letícia Gabrielle";
+$p9->altura =1.65;
+$p9->nascimento ="30-09-2006";
+$p9->hobbie = "Ouvir música e assistir série";
+
+// Camilly Vitória
+$p10 = new Pessoas();
+
+//Atribuir valores
+$p10->nome =" Camilly Vitória";
+$p10->altura =1.60;
+$p10->nascimento ="22-01-2007";
+$p10->hobbie = " Ouvir música/ fazer aula de jazz";
+
+//Guilherme Carvalho
+$p11 = new Pessoas();
+
+//Atribuir valores
+$p11->nome ="Guilherme Carvalho";
+$p11->altura =1.75;
+$p11->nascimento ="09-06-2007";
+$p11->hobbie = "Jogar";
+
+// Natally Reis
+$p12 = new Pessoas();
+
+//Atribuir valores
+$p12->nome =" Natally Reis";
+$p12->altura =1.60;
+$p12->nascimento ="07-01-2007";
+$p12->hobbie = " Ouvir música";
+
+//Pedro Grechi 
+$p13 = new Pessoas();
+
+//Atribuir valores
+$p13->nome ="Pedro Grechi ";
+$p13->altura =1.68;
+$p13->nascimento =" 23-03-2007";
+$p13->hobbie = "Jogar/ Desenhar";
+
+//Beatriz Baccaro
+$p14 = new Pessoas();
+
+//Atribuir valores
+$p14->nome ="Beatriz Baccaro";
+$p14->altura =1.60;
+$p14->nascimento ="02.08.2006";
+$p14->hobbie = "Esporte";
+
+//Luis Gustavo Magela
+$p15 = new Pessoas();
+
+//Atribuir valores
+$p15->nome ="Luis Gustavo Magela";
+$p15->altura =1.77;
+$p15->nascimento ="06-03-2007";
+$p15->hobbie = "Dormir e jogar videogame";
+
+//José Luiz Oliveira Costa 
+$p16 = new Pessoas();
+
+//Atribuir valores
+$p16->nome ="José Luiz Oliveira Costa ";
+$p16->altura =1.71;
+$p16->nascimento ="15-09-2006";
+$p16->hobbie = "Assistir/Crítica de filme";
+
+//Ryan Silveira
+$p17 = new Pessoas();
+
+//Atribuir valores
+$p17->nome ="Ryan Silveira";
+$p17->altura =1.72;
+$p17->nascimento ="02.09.2006";
+$p17->hobbie = "Jogar videogame";
+
+//Ana Clara Maciel
+$p18 = new Pessoas();
+
+//Atribuir valores
+$p18->nome ="Ana Clara Maciel";
+$p18->altura =1.50;
+$p18->nascimento ="19-01-2007";
+$p18->hobbie = " Ler e ouvir música";
+
+//Gustavo Otaviano
+$p19 = new Pessoas();
+
+//Atribuir valores
+$p19->nome ="Gustavo Otaviano";
+$p19->altura =1.85;
+$p19->nascimento ="04-03-2007";
+$p19->hobbie = "esportes";
+
+//Henrique Tavares
+$p20 = new Pessoas();
+
+//Atribuir valores
+$p20->nome ="Henrique Tavares";
+$p20->altura =1.63;
+$p20->nascimento ="26-07-2006";
+$p20->hobbie = "Jogar";
+
+//Gabriel Credendio
+$p21 = new Pessoas();
+
+//Atribuir valores
+$p21->nome ="Gabriel Credendio";
+$p21->altura =1.70;
+$p21->nascimento ="28-07-2006";
+$p21->hobbie = "Jogar";
+
+//Raí Franca
+$p22 = new Pessoas();
+
+//Atribuir valores
+$p22->nome ="Raí Franca";
+$p22->altura =1.82;
+$p22->nascimento ="12-09-2006";
+$p22->hobbie = "Jogar";
+
+//Ana Clara Ferreira 
+$p23 = new Pessoas();
+
+//Atribuir valores
+$p23->nome ="Ana Clara Ferreira ";
+$p23->altura =1.55;
+$p23->nascimento ="21-03-2007";
+$p23->hobbie = "Pintar";
+
+//Gabriel Alves 
+$p24 = new Pessoas();
+
+//Atribuir valores
+$p24->nome ="Gabriel Alves ";
+$p24->altura =1.84;
+$p24->nascimento ="03-08-2006";
+$p24->hobbie = "Praticar esportes";
+
+//Gabriel Dias
+$p25 = new Pessoas();
+
+//Atribuir valores
+$p25->nome ="Gabriel Dias";
+$p25->altura = 1.74;
+$p25->nascimento ="31-2-2506";
+$p25->hobbie = "Jogar";
+
+//Kauan Silva Vieira
+$p26 = new Pessoas();
+
+//Atribuir valores
+$p26->nome ="Kauan Silva Vieira";
+$p26->altura = 1.68;
+$p26->nascimento ="02-09-2006";
+$p26->hobbie = "Jogar videogame";
+
+//Beatriz Mayumi
+$p27 = new Pessoas();
+
+//Atribuir valores
+$p27->nome ="Beatriz Mayumi";
+$p27->altura = 1.65;
+$p27->nascimento ="03-03-2007";
+$p27->hobbie = "ler livros, jogar videogame e ouvir música.";
+
+//Duda
+$p28 = new Pessoas();
+
+//Atribuir valores
+$p28->nome ="Duda";
+$p28->altura = 1.65;
+$p28->nascimento ="02-04-2007";
+$p28->hobbie = "ver série e jogar vôlei.";
+
+//Giovana Lupo
+$p29 = new Pessoas();
+
+//Atribuir valores
+$p29->nome ="Giovana Lupo";
+$p29->altura = 1.62;
+$p29->nascimento ="14-05-2007";
+$p29->hobbie = "Pintar";
+
+//Bruno Alves
+$p30 = new Pessoas();
+
+//Atribuir valores
+$p30->nome ="Bruno Alves";
+$p30->altura = 1.78;
+$p30->nascimento ="06-12-2006";
+$p30->hobbie = "Academia";
+
+//Miguel Romero 
+$p31 = new Pessoas();
+
+//Atribuir valores
+$p31->nome ="Miguel Romero";
+$p31->altura = 1.70;
+$p31->nascimento ="14-04-2007";
+$p31->hobbie = "jogar futebol";
+
+//Henrique Nagata 
+$p32 = new Pessoas();
+
+//Atribuir valores
+$p32->nome ="Henrique Nagata";
+$p32->altura = 1.58;
+$p32->nascimento ="25-10-2006";
+$p32->hobbie = "andar de bicicleta";
+
+//Pedro Araújo 
+$p33 = new Pessoas();
+
+//Atribuir valores
+$p33->nome ="Pedro Araújo";
+$p33->altura = 1.68;
+$p33->nascimento ="24-09-2006";
+$p33->hobbie = "Jogar videogame";
+
+//Imprimir
+$p1->MostrarPessoas();
+echo "<br><hr>";
+$p2->MostrarPessoas();
+echo "<br><hr>";
+$p3->MostrarPessoas();
+echo "<br><hr>";  
+$p4->MostrarPessoas();
+echo "<br><hr>";
+$p5->MostrarPessoas();
+echo "<br><hr>";
+$p6->MostrarPessoas();
+echo "<br><hr>";
+$p7->MostrarPessoas();
+echo "<br><hr>";
+$p8->MostrarPessoas();
+echo "<br><hr>";
+$p9->MostrarPessoas();
+echo "<br><hr>";
+$p10->MostrarPessoas();
+echo "<br><hr>";
+$p11->MostrarPessoas();
+echo "<br><hr>";
+$p12->MostrarPessoas();
+echo "<br><hr>";
+$p13->MostrarPessoas();
+echo "<br><hr>";
+$p14->MostrarPessoas();
+echo "<br><hr>";
+$p15->MostrarPessoas();
+echo "<br><hr>";
+$p16->MostrarPessoas();
+echo "<br><hr>";
+$p17->MostrarPessoas();
+echo "<br><hr>";
+$p18->MostrarPessoas();
+echo "<br><hr>";
+$p19->MostrarPessoas();
+echo "<br><hr>";
+$p20->MostrarPessoas();
+echo "<br><hr>";
+$p21->MostrarPessoas();
+echo "<br><hr>";
+$p22->MostrarPessoas();
+echo "<br><hr>";
+$p23->MostrarPessoas();
+echo "<br><hr>";
+$p24->MostrarPessoas();
+echo "<br><hr>";
+$p25->MostrarPessoas();
+echo "<br><hr>";
+$p26->MostrarPessoas();
+echo "<br><hr>";
+$p27->MostrarPessoas();
+echo "<br><hr>";
+$p28->MostrarPessoas();
+echo "<br><hr>";
+$p29->MostrarPessoas();
+echo "<br><hr>";
+$p30->MostrarPessoas();
+echo "<br><hr>";
+$p31->MostrarPessoas();
+echo "<br><hr>";
+$p32->MostrarPessoas();
+echo "<br><hr>";
+$p33->MostrarPessoas();
+echo "<br><hr>";
+
+?>
---
+++ nome.class.php
@@ -0,0 +1,18 @@
+<?php
+Class Pessoas{
+
+    public $nome;
+    public $altura;
+    public $nascimento;
+    public $hobbie;
+
+
+    public function MostrarPessoas(){
+        echo "Nome: ".$this->nome."<br/>";
+        echo "Altura: ".$this->altura."<br/>";
+        echo "Nascimento: ".$this->nascimento."<br/>";
+        echo "Hobbie: ".$this->hobbie."<br/>";
+    }
+    
+}
+?>
